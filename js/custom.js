7cb23e9509a39de180fc94cae28cefc6ab84d683
diego <diego@isourcedigital.com>
2013-03-11 13:24:28 -0300

    fix

--- js/custom.js
+++ js/custom.js
@@ -21,11 +21,11 @@ $(document).ready(function(e) {
 	
 	var restaAlto1Ipad = 127;
 	var restaAlto1Iphone = 655;
-	var restaAlto1Ipod = 341;
+	var restaAlto1Ipod = 655;
 	
 	var restaAlto2Ipad = 75;
 	var restaAlto2Iphone = 635;
-	var restaAlto2Ipod = 390;
+	var restaAlto2Ipod = 635;
 	
 	/*--Alturas de los Devices---*/
 	
